8ef74e9709e2154931a761ca1f96c7433410edab
mengzg <mengzg@xiaoshouyi.com>
2017-10-31 18:24:03 +0800

    init

---
+++ springcloud-eureka/src/main/java/top/vota/App.java
@@ -0,0 +1,13 @@
+package top.vota;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
