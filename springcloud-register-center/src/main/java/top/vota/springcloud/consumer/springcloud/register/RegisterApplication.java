e196cc071f84d16c4d8a6f9f12fb849b1a04292a
mengzg <mengzg@xiaoshouyi.com>
2017-11-02 16:06:26 +0800

    commit

--- springcloud-register-center/src/main/java/top/vota/springcloud/consumer/springcloud/register/RegisterApplication.java
+++ springcloud-register-center/src/main/java/top/vota/springcloud/consumer/springcloud/register/RegisterApplication.java
@@ -1,4 +1,4 @@
-package top.vota.springcloud.register;
+package top.vota.springcloud.consumer.springcloud.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ springcloud-service-consumer/src/main/java/top/vota/springcloud/consumer/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package top.vota.springcloud.consumer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().web(true).run(args);
+    }
+}
